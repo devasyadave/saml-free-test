876b3180aeaead948b2daf9600e7bc2e397e2b55
miniOrangeDev <info@miniorange.com>
2019-05-22 11:41:18 +0530

    Composer require laravel 5.2+

---
+++ provider/ssoServiceProvider.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace provider;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Application;
+
+class ssoServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make('MiniOrange\Classes\Actions\AuthFacadeController');
+        
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $app = app();
+        $version = floatval($app->version());
+        if($version <= 5.3) {
+            if (! $this->app->routesAreCached()) {
+                require __DIR__.'/../src/routes.php';
+            }
+            $this->loadViewsFrom(__DIR__.'/../src/','newidea');
+
+
+        }
+        else{
+            $this->loadMigrationsFrom(__DIR__.'/../src/classes/actions');
+            $this->loadRoutesFrom(__DIR__.'/../src/routes.php');
+            $this->loadViewsFrom(__DIR__.'/../src/','newidea');
+        }
+    }
+}
